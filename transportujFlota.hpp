aae717d4c724c5fe3cb93fdd5c6fdb7479e66d43
Jędrzej <mr.myszer@gmail.com>
2024-11-03 17:48:07 +0100

    TEST

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,25 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    int      licz_zaglowcow = 0;
+    int      transport_calkowity = 0;
+    if (towar <= 0) {
+        return 0;
+    }
+    else {
+        while (transport_calkowity <= towar) 
+        {
+            Stocznia stocznia;
+            Statek*  s1 = stocznia();
+            if (dynamic_cast<Zaglowiec*>(s1) != nullptr) {
+                licz_zaglowcow++;
+            }
+            int obecny_transport = s1->transportuj();
+            transport_calkowity  = transport_calkowity + obecny_transport;
+            delete s1;
+        }
+    
+     return licz_zaglowcow;
+    }
+
 }
